beafc2422f7bbe79018824ff622ec65d60182596
Roger <roger.e.clayton@gmail.com>
2017-10-09 14:38:17 -0700

    adds subtitle

--- db/migrate/20170929134858_create_projects.rb
+++ db/migrate/20170929134858_create_projects.rb
@@ -3,6 +3,7 @@ class CreateProjects < ActiveRecord::Migration[5.1]
     create_table :projects do |t|
       t.string :title
       t.text :description
+      t.string :subtitle
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170929134858) do
   create_table "projects", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.string "subtitle"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
