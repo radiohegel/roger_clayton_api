5d60dabb38c1d14ef9efa2ccdd73dd9ef5733030
Roger <roger.e.clayton@gmail.com>
2018-04-11 14:24:40 -0600

    fixes typo in seeds

--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170929134858) do
     t.text "description"
     t.string "subtitle"
     t.string "githublink"
+    t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
